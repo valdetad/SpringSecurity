bee1b0e08d16ac1afbdb311d7286fb2f4ccd3595
valdetad <valdeta.demiri@hotmail.com>
2024-11-01 13:16:32 +0100

    - Created User entity with fields: id, username, password - Added @Data, @NoArgsConstructor, and @AllArgsConstructor annotations

--- SpringSecurity/src/main/java/com/example/SpringSecurity/data/model/User.java
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/data/model/User.java
@@ -1,4 +1,26 @@
 package com.example.SpringSecurity.data.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
 }
